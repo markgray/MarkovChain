0bf35b68dbce478bf1ea76138cfd7bb51d57a6a4
markgray <mark.t.gray@gmail.com>
2016-03-23 14:29:35 -0400

    Added output TextView for Markov result

--- mobile/src/main/java/com/example/android/markovchain/DoMarkov.java
+++ mobile/src/main/java/com/example/android/markovchain/DoMarkov.java
@@ -2,13 +2,17 @@ package com.example.android.markovchain;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TextView;
 
 public class DoMarkov extends Activity {
+
+    public TextView mTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_markov);
+        mTextview = (TextView) findViewById(R.id.textview_for_nonsense);
     }
 
 }
