675c1287cd20fbfb4695a63365cf6f390d679d2c
markgray <mark.t.gray@gmail.com>
2016-03-24 06:07:45 -0400

    Output Shakespeare.DIALOGUE to test ListView

--- mobile/src/main/java/com/example/android/markovchain/DoMarkov.java
+++ mobile/src/main/java/com/example/android/markovchain/DoMarkov.java
@@ -1,18 +1,28 @@
 package com.example.android.markovchain;
 
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.example.android.common.Shakespeare;
 
 public class DoMarkov extends ListActivity {
 
     public ListView mlistView;
+    public Cursor mCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_markov);
         mlistView = (ListView) findViewById(android.R.id.list);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Shakespeare.DIALOGUE);
+        mlistView.setAdapter(itemsAdapter);
     }
 
 }
