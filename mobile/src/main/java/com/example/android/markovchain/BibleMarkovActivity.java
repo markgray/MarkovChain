b1b64a8dc60e5a6cd12edce894a5c97d05952776
markgray <mark.t.gray@gmail.com>
2016-03-30 02:28:36 -0400

    Read in and display king_james_state_table

--- mobile/src/main/java/com/example/android/markovchain/BibleMarkovActivity.java
+++ mobile/src/main/java/com/example/android/markovchain/BibleMarkovActivity.java
@@ -1,18 +1,69 @@
 package com.example.android.markovchain;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class BibleMarkovActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BibleMarkovActivity extends ListActivity {
+    public final String TAG = "BibleMarkovActivity";
+    public ArrayList<String> tempList = new ArrayList<>();
+    public ListView mlistView;
+    protected String[] tempStr;
+    Random rand = new Random();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bible_markov);
+        mlistView = (ListView) findViewById(android.R.id.list);
+        InputStream inputStream = getApplicationContext().getResources().openRawResource(R.raw.king_james_state_table);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        fillArray(reader);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<>(this,
+                        android.R.layout.simple_list_item_1,
+                        tempStr);
+        mlistView.setAdapter(itemsAdapter);
+        mlistView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                int selection = Math.abs(rand.nextInt()) % tempStr.length;
+                mlistView.setSelection(selection);
+                Toast.makeText(getApplicationContext(), "Moving to verse " + selection, Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+
+    }
+    private void fillArray(BufferedReader reader) {
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                    tempList.add(line);
+            }
+            Log.i(TAG, "Verses read: " + tempList.size());
+            tempStr = new String[tempList.size()];
+            for (int i = 0; i < tempStr.length; i++) {
+                tempStr[i] = tempList.get(i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
