a0fcb4c1093f86a51086c2344df43088577142e5
markgray <mark.t.gray@gmail.com>
2016-05-06 13:38:42 -0400

    First stab at capital script

---
+++ common/src/main/res/raw/capital.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+open INP, '<king_james_caps_hash.txt';
+my %capHash = ();
+
+while (my $line = <INP>) {
+  chomp $line;
+  my @words = split / /, $line;
+  $capHash{$words[0]} = $words[1];
+}
+
+close INP;
+
+while (my $line =<>) {
+  chomp $line;
+  my @words = split / /, $line;
+  foreach my $word (@words) {
+    if (exists $capHash{$word}) {
+      $word = $capHash{$word};
+    }
+    print "$word ";
+  }
+  print "\n";
+}
+
+
