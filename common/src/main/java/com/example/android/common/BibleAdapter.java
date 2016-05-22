0e9f9897c4290b1ce40087932c7501c6d4e9d4f0
markgray <mark.t.gray@gmail.com>
2016-05-22 08:49:12 -0400

    Added code for book number to book name conversion

--- common/src/main/java/com/example/android/common/BibleAdapter.java
+++ common/src/main/java/com/example/android/common/BibleAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -68,6 +69,40 @@ public class BibleAdapter extends RecyclerView.Adapter<BibleAdapter.ViewHolder> 
         mDataSet = dataSet;
         mChapterAndVerse = chapterAndVerse;
         mLayoutManager = (LinearLayoutManager) layoutManager;
+    }
+
+    static public boolean numberToBookInitialized = false;
+    static public HashMap<String, String> numberToBook;
+    static final String[] numbers = {
+            "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12",
+            "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
+            "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36",
+            "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48",
+            "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
+            "61", "62", "63", "64", "65", "66"
+    };
+    static final String[] books = {
+            "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua",
+            "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings",
+            "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job",
+            "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah",
+            "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel",
+            "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah",
+            "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John",
+            "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians",
+            "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians",
+            "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter",
+            "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"
+    };
+    public HashMap<String, String> initializeNumberToBook() {
+        if (!numberToBookInitialized) {
+            for (int i = 0; i < numbers.length; i++) {
+                numberToBook.put(numbers[i], books[i]);
+            }
+            numberToBookInitialized = true;
+        }
+
+        return numberToBook;
     }
 
     // Create new views (invoked by the layout manager)
