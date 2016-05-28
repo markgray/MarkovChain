995bf7cd8a4acd93bfb1a1d6c23ed44a5747e62f
markgray <mark.t.gray@gmail.com>
2016-05-28 11:26:28 -0400

    Factored out BibleDialog into its own top level class

---
+++ mobile/src/main/java/com/example/android/markovchain/BibleDialog.java
@@ -0,0 +1,108 @@
+package com.example.android.markovchain;
+
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+public class BibleDialog extends DialogFragment {
+    String mLabel;
+    String mText;
+
+    /**
+     * Create and iniitialize a BibleDialog DialogFragment
+     *
+     * @param label Label to use
+     * @param text  Text to use
+     * @return Initialized BibleDialog
+     */
+    static BibleDialog newInstance(String label, String text) {
+        BibleDialog f = new BibleDialog();
+
+        Bundle args = new Bundle();
+        args.putString("label", label);
+        args.putString("text", text);
+
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLabel = getArguments().getString("label");
+        mText = getArguments().getString("text");
+
+        setStyle(DialogFragment.STYLE_NORMAL, 0);
+    }
+
+    public String[] spinChoices = {"Dismiss", "Random verse", "Google", "Bookmark", "Go to verse"};
+    public String spinChosen = "";
+    public int spinIndex = 0;
+    public static final int CHOICE_DISMISS = 0;
+    public static final int CHOICE_RANDOM_VERSE = 1;
+    public static final int CHOICE_GOOGLE = 2;
+    public static final int CHOICE_BOOKMARK = 3;
+    public static final int CHOICE_GO_TO_VERSE = 4;
+
+    public AdapterView.OnItemSelectedListener spinSelected = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            spinIndex = position;
+            spinChosen = spinChoices[position];
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_dialog, container, false);
+        View tv = v.findViewById(R.id.label);
+        String dialogLabel = mLabel;
+        ((TextView) tv).setText(dialogLabel);
+
+        tv = v.findViewById(R.id.text);
+        ((TextView) tv).setText(mText);
+
+
+        Spinner spin = (Spinner) v.findViewById(R.id.spinner);
+
+        ArrayAdapter<String> spinnerArrayAdapter =
+                new ArrayAdapter<>(v.getContext(),
+                        android.R.layout.simple_spinner_item,
+                        spinChoices);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(spinnerArrayAdapter);
+        spin.setOnItemSelectedListener(spinSelected);
+
+        // Watch for button clicks.
+        Button button = (Button) v.findViewById(R.id.show);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // When button is clicked, call up to owning activity.
+                ((BibleFragment) getActivity()).handleAction(v, spinIndex);
+                ((BibleFragment) getActivity()).dismissDiaglog();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public int show(FragmentTransaction ft, String dialog) {
+        return super.show(ft, dialog);
+    }
+}
