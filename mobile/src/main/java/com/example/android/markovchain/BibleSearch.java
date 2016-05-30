d76b7a38b4a8fa23d535b3e5df889fe0b092bfe8
markgray <mark.t.gray@gmail.com>
2016-05-30 09:07:08 -0400

    Added skeleton BibleSearch

---
+++ mobile/src/main/java/com/example/android/markovchain/BibleSearch.java
@@ -0,0 +1,22 @@
+package com.example.android.markovchain;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BibleSearch extends DialogFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
