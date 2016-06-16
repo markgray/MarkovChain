37990659e107fcba76f7ea2345e6275f9ffa332e
markgray <mark.t.gray@gmail.com>
2016-06-16 07:03:25 -0400

    Removed unused code

--- mobile/src/main/java/com/example/android/markovchain/BibleMain.java
+++ mobile/src/main/java/com/example/android/markovchain/BibleMain.java
@@ -140,7 +140,6 @@ public class BibleMain extends Activity {
                 break;
             case BibleDialog.CHOICE_RANDOM_VERSE:
                 BibleAdapter.moveToRandom(v);
-//                showDialog(BibleDialog.newInstance(dialogTitle, dialogText));
                 bibleDialog.refresh(dialogTitle, dialogText);
                 break;
             case BibleDialog.CHOICE_GOOGLE:
