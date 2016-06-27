16c199b5888d6be8aae5f3a9ea39e032822f3fe6
markgray <mark.t.gray@gmail.com>
2016-06-27 06:08:09 -0400

    Added comments

--- mobile/src/main/java/com/example/android/markovchain/BibleSpeak.java
+++ mobile/src/main/java/com/example/android/markovchain/BibleSpeak.java
@@ -35,6 +35,25 @@ public class BibleSpeak extends DialogFragment implements OnInitListener {
         return f;
     }
 
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null (which
+     * is the default implementation).  This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+     *
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the View for the fragment's UI, or null.
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,8 +61,8 @@ public class BibleSpeak extends DialogFragment implements OnInitListener {
         mView = inflater.inflate(R.layout.bible_speak, container, false);
         setDisplayedText(mView);
 
-        // Watch for button clicks.
-        Button button = (Button) mView.findViewById(R.id.dissmiss);
+        // Watch for dismiss button clicks
+        Button button = (Button) mView.findViewById(R.id.dismiss);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // When button is clicked, dismiss this DialogFragment
@@ -52,6 +71,7 @@ public class BibleSpeak extends DialogFragment implements OnInitListener {
                 BibleMain.bibleDialog.mText = BibleMain.dialogText;
             }
         });
+        // Watch for NEXT  button clicks
         button = (Button) mView.findViewById(R.id.next);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -68,6 +88,11 @@ public class BibleSpeak extends DialogFragment implements OnInitListener {
         return mView;
     }
 
+    /**
+     * Set the label and text of the view to those saved in mLabel and mText
+     *
+     * @param v Main View for the DialogFragment (usually mView)
+     */
     public void setDisplayedText(View v) {
         View tv = v.findViewById(R.id.label);
         String dialogLabel = mLabel;
@@ -77,6 +102,19 @@ public class BibleSpeak extends DialogFragment implements OnInitListener {
         ((TextView) tv).setText(mText);
     }
 
+    /**
+     * Called to do initial creation of a fragment.  This is called after
+     * onAttach(Activity) and before onCreateView(LayoutInflater, ViewGroup, Bundle).
+     *
+     * <p>Note that this can be called while the fragment's activity is
+     * still in the process of being created.  As such, you can not rely
+     * on things like the activity's content view hierarchy being initialized
+     * at this point.  If you want to do work once the activity itself is
+     * created, see {@link #onActivityCreated(Bundle)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     * a previous saved state, this is the state.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,6 +128,11 @@ public class BibleSpeak extends DialogFragment implements OnInitListener {
 
     }
 
+    /**
+     * Called to signal the completion of the TextToSpeech engine initialization.
+     *
+     * @param status {@link TextToSpeech#SUCCESS} or {@link TextToSpeech#ERROR}.
+     */
     @Override
     public void onInit(int status) {
         if (status == TextToSpeech.SUCCESS) {
