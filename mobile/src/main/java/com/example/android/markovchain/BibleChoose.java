f4bb305457a599295d126d527dae11fa20710987
markgray <mark.t.gray@gmail.com>
2016-07-06 05:55:51 -0400

    Added comments

--- mobile/src/main/java/com/example/android/markovchain/BibleChoose.java
+++ mobile/src/main/java/com/example/android/markovchain/BibleChoose.java
@@ -137,6 +137,18 @@ public class BibleChoose extends DialogFragment {
     public int bookNumber = 0;
     public String bookName;
     public AdapterView.OnItemSelectedListener spinSelected = new AdapterView.OnItemSelectedListener() {
+        /**
+         * Callback method to be invoked when an item in this view has been selected.
+         * This callback is invoked only when the newly selected position is different
+         * from the previously selected position or if there was no selected item.
+         * Impelmenters can call getItemAtPosition(position) if they need to access
+         * the data associated with the selected item.
+         *
+         * @param parent The AdapterView where the selection happened
+         * @param view The view within the AdapterView that was clicked
+         * @param position The position of the view in the adapter
+         * @param id The row id of the item that is selected
+         */
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
             bookNumber = position;
@@ -144,6 +156,13 @@ public class BibleChoose extends DialogFragment {
 
         }
 
+        /**
+         * Callback method to be invoked when the selection disappears from this view.
+         * The selection can disappear for instance when touch is activated or when
+         * the adapter becomes empty.
+         *
+         * @param parent The AdapterView that now contains no selected item.
+         */
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
         }
