06b0349d7d1696fd9e142fa95e7e21f06ec5ebac
markgray <mark.t.gray@gmail.com>
2016-07-10 05:44:21 -0400

    Cleaned up CalcTask to make it easier to use.

--- common/src/main/java/com/example/android/common/CalcTask.java
+++ common/src/main/java/com/example/android/common/CalcTask.java
@@ -5,26 +5,39 @@ import android.os.AsyncTask;
 /**
  * Background task to be benchmarked
 */
-public class CalcTask extends AsyncTask<Long, Long, Double> {
+public class CalcTask extends AsyncTask<Long, Long, Long> {
+
+    public BenchMark benchMark = new BenchMark();
+    double acc = 1.000000001;
+    double div = 0.999999999;
 
     /**
-     * Override this method to perform a computation on a background thread.
+     * This method performs a computation contained in the method testMethod
+     * reps times on a background thread.
      * The specified parameters are the parameters passed to execute(Params...)
      * by the caller of this task. This method can call publishProgress(Progress...)
      * to publish updates on the UI thread.
      *
      * @param reps Number of repetitions the for loop should execute
-     * @return Result of the computation.
+     * @return Time in milliseconds that the benchmark took
      */
     @Override
-    protected Double doInBackground(Long... reps) {
+    protected Long doInBackground(Long... reps) {
         Long repeats = reps[0];
-        double acc = 1.000000001;
-        double div = 0.999999999;
+
+        benchMark.Start();
         for (int i = 0; i < repeats ; i++) {
-            acc = acc / div;
+            testMethod();
         }
-        return acc;
+        return benchMark.Stop();
+    }
+
+    /**
+     * This method should be overridden by a method which performs whatever computation
+     * you wish to benchmark.
+     */
+    public void testMethod() {
+        acc = acc / div;
     }
 
 }
