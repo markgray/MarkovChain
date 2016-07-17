c0d049df29c41591d7b2eba9447d197274ab4c7a
markgray <mark.t.gray@gmail.com>
2016-07-17 06:00:49 -0400

    Tested the code whose speed difference I was curious about. Eek! my fears were right!

--- mobile/src/main/java/com/example/android/markovchain/TestBenchMark.java
+++ mobile/src/main/java/com/example/android/markovchain/TestBenchMark.java
@@ -11,8 +11,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.common.CalcTask;
+import com.example.android.common.Shakespeare;
 
 import java.text.NumberFormat;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Locale;
 
 public class TestBenchMark extends Activity {
@@ -27,7 +30,7 @@ public class TestBenchMark extends Activity {
     LinearLayout mResultsLinearLayout;
     ControlClass mControlInstance;
     final Long PROGRESS_STEPS = 100L;
-    final Long LOOP_REPITIONS = 10000000L;
+    final Long LOOP_REPITIONS = 100L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +42,7 @@ public class TestBenchMark extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "Start button clicked");
-                mControlInstance = new ControlClass1();
+                mControlInstance = new ControlClass3();
                 mControlInstance.execute(LOOP_REPITIONS, PROGRESS_STEPS);
             }
         });
@@ -48,7 +51,7 @@ public class TestBenchMark extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "Start button clicked");
-                mControlInstance = new ControlClass2();
+                mControlInstance = new ControlClass4();
                 mControlInstance.execute(LOOP_REPITIONS, PROGRESS_STEPS);
             }
         });
@@ -94,30 +97,96 @@ public class TestBenchMark extends Activity {
         }
     }
 
-    private class ControlClass1 extends ControlClass {
-        public double acc = 1.000000001;
-        public double div = 0.999999999;
+//    private class ControlClass1 extends ControlClass {
+//        public double acc = 1.000000001;
+//        public double div = 0.999999999;
+//        /**
+//         * This method should be overridden by a method which performs whatever computation
+//         * you wish to benchmark.
+//         */
+//        @Override
+//        public void testMethod() {
+//            acc = acc / div;
+//        }
+//    }
+//
+//    private class ControlClass2 extends ControlClass {
+//        public double acc = 1.000000001;
+//        public double mul = 0.999999999;
+//        /**
+//         * This method should be overridden by a method which performs whatever computation
+//         * you wish to benchmark.
+//         */
+//        @Override
+//        public void testMethod() {
+//            acc = acc * mul;
+//        }
+//    }
+
+    private class ControlClass3 extends ControlClass {
+
+        public String[] testArray;
+
+        public ControlClass3() {
+            testArray = Shakespeare.SONNETS[0].split(" ");
+        }
+        /**
+         * Given an array of strings which might contain duplicate strings remove all
+         * duplicates
+         *
+         * @param strings String array with possible duplicate string members
+         * @return Same array with only single occurrences of strings
+         */
+        private String[] uniq(String[] strings) {
+            HashSet<String> setOfStrings = new HashSet<>(strings.length);
+            Collections.addAll(setOfStrings, strings);
+            String[] returnStringArray = new String[setOfStrings.size()];
+            for (int i = 0; i < setOfStrings.size(); i++) {
+                returnStringArray[i] = (String) setOfStrings.toArray()[i];
+            }
+            return returnStringArray;
+        }
         /**
          * This method should be overridden by a method which performs whatever computation
          * you wish to benchmark.
          */
         @Override
         public void testMethod() {
-            acc = acc / div;
+            testArray = uniq(testArray);
         }
 
     }
+    private class ControlClass4 extends ControlClass {
 
-    private class ControlClass2 extends ControlClass {
-        public double acc = 1.000000001;
-        public double mul = 0.999999999;
+        public String[] testArray;
+
+        public ControlClass4() {
+            testArray = Shakespeare.SONNETS[0].split(" ");
+        }
+        /**
+         * Given an array of strings which might contain duplicate strings remove all
+         * duplicates
+         *
+         * @param strings String array with possible duplicate string members
+         * @return Same array with only single occurrences of strings
+         */
+        private String[] uniq(String[] strings) {
+            HashSet<String> setOfStrings = new HashSet<>(strings.length);
+            Collections.addAll(setOfStrings, strings);
+            String[] returnStringArray = new String[setOfStrings.size()];
+            Object[] tempObjectArray = setOfStrings.toArray();
+            for (int i = 0; i < setOfStrings.size() ; i++) {
+                returnStringArray[i] = (String) tempObjectArray[i];
+            }
+            return returnStringArray;
+        }
         /**
          * This method should be overridden by a method which performs whatever computation
          * you wish to benchmark.
          */
         @Override
         public void testMethod() {
-            acc = acc * mul;
+            testArray = uniq(testArray);
         }
 
     }
