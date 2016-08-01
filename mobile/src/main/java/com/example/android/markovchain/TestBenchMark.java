b12909dd83794aac0559dbc5baa64a95adc497ec
markgray <mark.t.gray@gmail.com>
2016-08-01 06:26:16 -0400

    Added comments

--- mobile/src/main/java/com/example/android/markovchain/TestBenchMark.java
+++ mobile/src/main/java/com/example/android/markovchain/TestBenchMark.java
@@ -11,11 +11,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.common.CalcTask;
-import com.example.android.common.Shakespeare;
+//import com.example.android.common.Shakespeare;
 
 import java.text.NumberFormat;
-import java.util.Collections;
-import java.util.HashSet;
+//import java.util.Collections;
+//import java.util.HashSet;
 import java.util.Locale;
 
 /**
@@ -33,7 +33,7 @@ public class TestBenchMark extends Activity {
     LinearLayout mResultsLinearLayout;
     ControlClass mControlInstance;
     final Long PROGRESS_STEPS = 100L;
-    final Long LOOP_REPITIONS = 100L;
+    final Long LOOP_REPITIONS = 10000000L;
 
     /**
      * Called when the activity is starting, it sets the content view to the layout
@@ -44,7 +44,13 @@ public class TestBenchMark extends Activity {
      * run the second method's benchmark. It then locates the Button R.id.abort and sets its
      * OnClickListener to just Log a message TODO: Implement an "abort" function
      * It locates the LinearLayout containing the ProgressBar (R.id.progress_view_linear_layout)
-     * and squirrels it away in mProgressLayout to use later to swap visibility of views.
+     * and the LinearLayout containing the result View (R.id.results_linear_layout) and squirrels
+     * them away in mProgressLayout, and mResultsLinearLayout to use later to swap visibility of
+     * these two views. It locates the "TRY AGAIN" button in the results LinearLayout and set its
+     * OnClickListener to swap the visibility from the results LinearLayout to the progress
+     * LinearLayout. It then locates the TextView for the results of the benchmark (R.id.results_view)
+     * and squirrels it away in mResults for use by the benchmarking to append the output of the
+     * results.
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden
      */
@@ -58,7 +64,7 @@ public class TestBenchMark extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "Start button clicked");
-                mControlInstance = new ControlClass3();
+                mControlInstance = new ControlClass1();
                 mControlInstance.execute(LOOP_REPITIONS, PROGRESS_STEPS);
             }
         });
@@ -67,7 +73,7 @@ public class TestBenchMark extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "Start button clicked");
-                mControlInstance = new ControlClass4();
+                mControlInstance = new ControlClass2();
                 mControlInstance.execute(LOOP_REPITIONS, PROGRESS_STEPS);
             }
         });
@@ -79,6 +85,8 @@ public class TestBenchMark extends Activity {
             }
         });
         mProgressLayout = (LinearLayout) findViewById(R.id.progress_view_linear_layout);
+        mResultsLinearLayout = (LinearLayout) findViewById(R.id.results_linear_layout);
+
         mTryAgain = (Button) findViewById(R.id.try_again);
         mTryAgain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,7 +95,6 @@ public class TestBenchMark extends Activity {
                 mProgressLayout.setVisibility(View.VISIBLE);
             }
         });
-        mResultsLinearLayout = (LinearLayout) findViewById(R.id.results_linear_layout);
 
         mResults = (TextView) findViewById(R.id.results_view);
     }
@@ -113,98 +120,98 @@ public class TestBenchMark extends Activity {
         }
     }
 
-//    private class ControlClass1 extends ControlClass {
-//        public double acc = 1.000000001;
-//        public double div = 0.999999999;
-//        /**
-//         * This method should be overridden by a method which performs whatever computation
-//         * you wish to benchmark.
-//         */
-//        @Override
-//        public void testMethod() {
-//            acc = acc / div;
-//        }
-//    }
+    private class ControlClass1 extends ControlClass {
+        public double acc = 1.000000001;
+        public double div = 0.999999999;
+        /**
+         * This method should be overridden by a method which performs whatever computation
+         * you wish to benchmark.
+         */
+        @Override
+        public void testMethod() {
+            acc = acc / div;
+        }
+    }
+
+    private class ControlClass2 extends ControlClass {
+        public double acc = 1.000000001;
+        public double mul = 0.999999999;
+        /**
+         * This method should be overridden by a method which performs whatever computation
+         * you wish to benchmark.
+         */
+        @Override
+        public void testMethod() {
+            acc = acc * mul;
+        }
+    }
+
+//    private class ControlClass3 extends ControlClass {
 //
-//    private class ControlClass2 extends ControlClass {
-//        public double acc = 1.000000001;
-//        public double mul = 0.999999999;
+//        public String[] testArray;
+//
+//        public ControlClass3() {
+//            testArray = Shakespeare.SONNETS[0].split(" ");
+//        }
+//        /**
+//         * Given an array of strings which might contain duplicate strings remove all
+//         * duplicates
+//         *
+//         * @param strings String array with possible duplicate string members
+//         * @return Same array with only single occurrences of strings
+//         */
+//        private String[] uniq(String[] strings) {
+//            HashSet<String> setOfStrings = new HashSet<>(strings.length);
+//            Collections.addAll(setOfStrings, strings);
+//            String[] returnStringArray = new String[setOfStrings.size()];
+//            for (int i = 0; i < setOfStrings.size(); i++) {
+//                returnStringArray[i] = (String) setOfStrings.toArray()[i];
+//            }
+//            return returnStringArray;
+//        }
 //        /**
 //         * This method should be overridden by a method which performs whatever computation
 //         * you wish to benchmark.
 //         */
 //        @Override
 //        public void testMethod() {
-//            acc = acc * mul;
+//            testArray = uniq(testArray);
 //        }
+//
 //    }
-
-    private class ControlClass3 extends ControlClass {
-
-        public String[] testArray;
-
-        public ControlClass3() {
-            testArray = Shakespeare.SONNETS[0].split(" ");
-        }
-        /**
-         * Given an array of strings which might contain duplicate strings remove all
-         * duplicates
-         *
-         * @param strings String array with possible duplicate string members
-         * @return Same array with only single occurrences of strings
-         */
-        private String[] uniq(String[] strings) {
-            HashSet<String> setOfStrings = new HashSet<>(strings.length);
-            Collections.addAll(setOfStrings, strings);
-            String[] returnStringArray = new String[setOfStrings.size()];
-            for (int i = 0; i < setOfStrings.size(); i++) {
-                returnStringArray[i] = (String) setOfStrings.toArray()[i];
-            }
-            return returnStringArray;
-        }
-        /**
-         * This method should be overridden by a method which performs whatever computation
-         * you wish to benchmark.
-         */
-        @Override
-        public void testMethod() {
-            testArray = uniq(testArray);
-        }
-
-    }
-    private class ControlClass4 extends ControlClass {
-
-        public String[] testArray;
-
-        public ControlClass4() {
-            testArray = Shakespeare.SONNETS[0].split(" ");
-        }
-        /**
-         * Given an array of strings which might contain duplicate strings remove all
-         * duplicates
-         *
-         * @param strings String array with possible duplicate string members
-         * @return Same array with only single occurrences of strings
-         */
-        private String[] uniq(String[] strings) {
-            HashSet<String> setOfStrings = new HashSet<>(strings.length);
-            Collections.addAll(setOfStrings, strings);
-            String[] returnStringArray = new String[setOfStrings.size()];
-            Object[] tempObjectArray = setOfStrings.toArray();
-            for (int i = 0; i < setOfStrings.size() ; i++) {
-                returnStringArray[i] = (String) tempObjectArray[i];
-            }
-            return returnStringArray;
-        }
-        /**
-         * This method should be overridden by a method which performs whatever computation
-         * you wish to benchmark.
-         */
-        @Override
-        public void testMethod() {
-            testArray = uniq(testArray);
-        }
-
-    }
+//    private class ControlClass4 extends ControlClass {
+//
+//        public String[] testArray;
+//
+//        public ControlClass4() {
+//            testArray = Shakespeare.SONNETS[0].split(" ");
+//        }
+//        /**
+//         * Given an array of strings which might contain duplicate strings remove all
+//         * duplicates
+//         *
+//         * @param strings String array with possible duplicate string members
+//         * @return Same array with only single occurrences of strings
+//         */
+//        private String[] uniq(String[] strings) {
+//            HashSet<String> setOfStrings = new HashSet<>(strings.length);
+//            Collections.addAll(setOfStrings, strings);
+//            String[] returnStringArray = new String[setOfStrings.size()];
+//            Object[] tempObjectArray = setOfStrings.toArray();
+//            for (int i = 0; i < setOfStrings.size() ; i++) {
+//                returnStringArray[i] = (String) tempObjectArray[i];
+//            }
+//            return returnStringArray;
+//        }
+//        /**
+//         * This method should be overridden by a method which performs whatever computation
+//         * you wish to benchmark.
+//         */
+//        @Override
+//        public void testMethod() {
+//            testArray = uniq(testArray);
+//        }
+//
+//    }
 
 }
