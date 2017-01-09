3ec65b64c354a83c3927e14fd0128a5a71bddd03
markgray <mark.t.gray@gmail.com>
2017-01-09 05:19:18 -0500

    Fleshed out MarkovStats a little

--- mobile/src/main/java/com/example/android/markovchain/MarkovStats.java
+++ mobile/src/main/java/com/example/android/markovchain/MarkovStats.java
@@ -13,4 +13,14 @@ public class MarkovStats {
         variations.add(var);
         return this;
     }
+
+    public String toString() {
+        StringBuilder returnString = new StringBuilder();
+        int totalVariations = 1;
+        for (Integer choices :variations) {
+            returnString.append(choices).append("x");
+            totalVariations *= choices.intValue();
+        }
+        return returnString.toString() + totalVariations;
+    }
 }
