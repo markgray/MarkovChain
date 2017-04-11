55146fe5cdec664f9b3de6450979583d14ffc134
markgray <mark.t.gray@gmail.com>
2017-04-11 07:41:15 -0400

    Made ClockDataItem Comparable (I think)

--- common/src/main/java/com/example/android/common/ClockDataItem.java
+++ common/src/main/java/com/example/android/common/ClockDataItem.java
@@ -1,9 +1,12 @@
 package com.example.android.common;
 
+import java.util.Comparator;
+
 /**
  * Contains data and methods for holding a time of day, and the corresponding clock hand angles.
  */
-public class ClockDataItem {
+@SuppressWarnings("WeakerAccess")
+public class ClockDataItem implements Comparator {
 
     public int timeHour;
     public int timeMinute;
@@ -16,5 +19,14 @@ public class ClockDataItem {
         timeHour = hour;
         timeMinute = minute;
         timeSecond = second;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        ClockDataItem clock1 = (ClockDataItem) o1;
+        ClockDataItem clock2 = (ClockDataItem) o2;
+        Double pieSlice1 = Math.abs(clock1.angleHour - clock1.angleMinute);
+        Double pieSlice2 = Math.abs(clock2.angleHour - clock2.angleMinute);
+        return Double.compare(pieSlice1 - 120.0, pieSlice2 - 120.0);
     }
 }
