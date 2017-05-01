031453b91602f60f76686cbd564124d25f60ab39
markgray <mark.t.gray@gmail.com>
2017-05-01 04:08:00 -0400

    Added more skeleton overrides in ClockDataTask

--- mobile/src/main/java/com/example/android/markovchain/ClockDataTask.java
+++ mobile/src/main/java/com/example/android/markovchain/ClockDataTask.java
@@ -9,16 +9,102 @@ import com.example.android.common.ClockDataItem;
  * sorted by the {@code badness()} results of each.
  */
 public class ClockDataTask extends AsyncTask<ClockDataItem, ClockDataItem, ClockDataItem> {
+    public ClockDataItem clock = new ClockDataItem(12, 0, 0.0);
+    /**
+     * Runs on the UI thread before {@link #doInBackground}.
+     *
+     * @see #onPostExecute
+     * @see #doInBackground
+     */
     @Override
-    protected ClockDataItem doInBackground(ClockDataItem... params) {
-        return null;
+    protected void onPreExecute() {
+        super.onPreExecute();
     }
 
+    /**
+     * Override this method to perform a computation on a background thread. The
+     * specified parameters are the parameters passed to {@link #execute}
+     * by the caller of this task.
+     *
+     * This method can call {@link #publishProgress} to publish updates
+     * on the UI thread.
+     *
+     * @param params The parameters of the task.
+     *
+     * @return A result, defined by the subclass of this task.
+     *
+     * @see #onPreExecute()
+     * @see #onPostExecute
+     * @see #publishProgress
+     */
+    @Override
+    protected ClockDataItem doInBackground(ClockDataItem... params) {
+        publishProgress(clock);
+        return clock;
+    }
+
+    /**
+     * Runs on the UI thread after {@link #cancel(boolean)} is invoked and
+     * {@link #doInBackground(Object[])} has finished.
+     *
+     * The default implementation simply invokes {@link #onCancelled()} and
+     * ignores the result. If you write your own implementation, do not call
+     * {@code super.onCancelled(result)}
+     *
+     * @param clockDataItem The result, if any, computed in {@link #doInBackground(Object[])}
+     *                      , can be null
+     *
+     * @see #cancel(boolean)
+     * @see #isCancelled()
+     */
+    @Override
+    protected void onCancelled(ClockDataItem clockDataItem) {
+        super.onCancelled(clockDataItem);
+    }
+
+    /**
+     * Applications should preferably override {@link #onCancelled(Object)}.
+     * This method is invoked by the default implementation of
+     * {@link #onCancelled(Object)}.
+     *
+     * Runs on the UI thread after {@link #cancel(boolean)} is invoked and
+     * {@link #doInBackground(Object[])} has finished.
+     *
+     * @see #onCancelled(Object)
+     * @see #cancel(boolean)
+     * @see #isCancelled()
+     */
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
+
+    /**
+     * Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     *
+     * This method won't be invoked if the task was cancelled.
+     *
+     * @param aClockDataItem The result of the operation computed by {@link #doInBackground}.
+     *
+     * @see #onPreExecute
+     * @see #doInBackground
+     * @see #onCancelled(Object)
+     */
     @Override
     protected void onPostExecute(ClockDataItem aClockDataItem) {
         super.onPostExecute(aClockDataItem);
     }
 
+    /**
+     * Runs on the UI thread after {@link #publishProgress} is invoked.
+     * The specified values are the values passed to {@link #publishProgress}.
+     *
+     * @param values The values indicating progress.
+     *
+     * @see #publishProgress
+     * @see #doInBackground
+     */
     @Override
     protected void onProgressUpdate(ClockDataItem... values) {
         super.onProgressUpdate(values);
