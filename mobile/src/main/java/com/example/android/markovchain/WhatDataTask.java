ef297a3da0d117afa3bb651b1a7afbf6444e5572
markgray <mark.t.gray@gmail.com>
2019-02-08 06:40:04 -0500

    Added comments

--- mobile/src/main/java/com/example/android/markovchain/WhatDataTask.java
+++ mobile/src/main/java/com/example/android/markovchain/WhatDataTask.java
@@ -11,26 +11,54 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * An {@code AsyncTask} which loads the Html file with the resource ID specified by the parameter
+ * passed to the method {@code doInBackground} in the background and returns a {@code Spanned} string
+ * object to the caller.
+ */
 @SuppressWarnings("WeakerAccess")
 public class WhatDataTask extends AsyncTask<Integer, String, Spanned> {
+    /**
+     * {@code Context} to use to access resources from of application (in our case this is the
+     * "context of the single, global Application object of the current process" obtained from the
+     * {@code getApplicationContext} method of the {@code WhatIsMan} activity and then passed to our
+     * constructor).
+     */
     @SuppressLint("StaticFieldLeak")
     Context mContext;
+
+    /**
+     * Our constructor, we just save our parameter {@code Context context} in our field {@code mContext}.
+     *
+     * @param context {@code Context} to use to access resources of this application
+     */
     WhatDataTask(Context context) {
         mContext = context;
     }
+
     /**
-     * Override this method to perform a computation on a background thread. The
-     * specified parameters are the parameters passed to {@link #execute}
-     * by the caller of this task.
+     * Loads a Html file from our resources on a background thread and returns a {@code Spanned} string
+     * created from the contents of the file we load. The parameter is the resource ID of the file
+     * passed to {@link #execute} by the caller of this task. First we initialize our variable
+     * {@code StringBuilder builder} with a new instance, and declare our variable {@code String line}.
+     * We initialize {@code InputStream inputStream} by fetching a {@code Resources} instance for the
+     * application's package as returned by the {@code getResources} method of our field
+     * {@code mContext}, and using the {@code Resources} instance open a data stream for reading the
+     * raw resource with resource ID {@code resourceId[0]}. Next we initialize {@code BufferedReader reader}
+     * with a buffering character-input stream that uses a default-sized input buffer to read from an
+     * {@code InputStreamReader} constructed to read bytes from {@code inputStream} and decode them
+     * into characters using  the platform's default charset.
      * <p>
-     * This method can call {@link #publishProgress} to publish updates
-     * on the UI thread.
+     * Having set everything up, wrapped in a try block intended to catch and log IOException, we loop
+     * setting {@code line} to the {@code String} returned by the {@code readLine} method of {@code reader}
+     * until it returns null appending each {@code line} to {@code builder}. When done reading the
+     * entire file into {@code builder} we close {@code reader}.
+     * <p>
+     * Upon exiting from the try block we return the {@code Spanned} string created by the {@code fromHtml}
+     * method of {@code Html} from the string value of {@code builder}.
      *
-     * @param resourceId The parameters of the task.
-     * @return A result, defined by the subclass of this task.
-     * @see #onPreExecute()
-     * @see #onPostExecute
-     * @see #publishProgress
+     * @param resourceId the resource ID of the Html file we are to load.
+     * @return A {@code Spanned} string created from the contents of the file we load.
      */
     @Override
     protected Spanned doInBackground(Integer... resourceId) {
--- mobile/src/main/java/com/example/android/markovchain/WhatIsMan.java
+++ mobile/src/main/java/com/example/android/markovchain/WhatIsMan.java
@@ -109,6 +109,14 @@ public class WhatIsMan extends Activity {
         Button button = new Button(this);
         button.setText(description);
         button.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Called when the {@code Button} is clicked. We just set the visibility of our field
+             * {@code ScrollView whatChapterScrollView} to GONE (disappears our chapter selection
+             * buttons) and call our method {@code loadResourceHtml} to load the file whose resource
+             * ID is that given by the {@code addButton} method's parameter {@code resourceID}.
+             *
+             * @param v {@code View} that was clicked.
+             */
             @Override
             public void onClick(View v) {
                 whatChapterScrollView.setVisibility(View.GONE);
@@ -118,19 +126,30 @@ public class WhatIsMan extends Activity {
         parent.addView(button);
     }
 
+    /**
+     * Loads the Html file with the resource ID of our parameter {@code int resourceID} in the background
+     * then displays it in our field {@code TextView whatTextView} when it is done loading. We initialize
+     * our variable {@code WhatDataTask mWhatDataTask} with a new instance, whose {@code onPostExecute}
+     * override sets the text of {@code whatTextView} to the {@code Spanned} returned from the method
+     * {@code doInBackground} of {@code mWhatDataTask}, sets the visibility of {@code TextView whatWaiting}
+     * to GONE, and sets the visibility of {@code whatTextView} to VISIBLE. Having done this we call the
+     * {@code execute} method of {@code mWhatDataTask} (which in turn calls the {@code doInBackground}
+     * method in a separate thread) to have it load the file whose resource ID is our parameter
+     * {@code resourceID}.
+     *
+     * @param resourceID resource ID of Html file located in our raw resources.
+     */
     private void loadResourceHtml(int resourceID) {
         @SuppressLint("StaticFieldLeak")
         WhatDataTask mWhatDataTask = new WhatDataTask(getApplicationContext()) {
             /**
-             * <p>Runs on the UI thread after {@link #doInBackground}. The
-             * specified result is the value returned by {@link #doInBackground}.</p>
-             *
-             * <p>This method won't be invoked if the task was cancelled.</p>
+             * Runs on the UI thread after {@link #doInBackground}. The parameter {@code Spanned s}
+             * is the value returned by {@link #doInBackground}. We set the text of our field
+             * {@code TextView whatTextView} to our parameter {@code s}, set the visibility of our
+             * field {@code TextView whatWaiting} to GONE, then set the visibility of {@code whatTextView}
+             * to VISIBLE.
              *
              * @param s The result of the operation computed by {@link #doInBackground}.
-             * @see #onPreExecute
-             * @see #doInBackground
-             * @see #onCancelled(Object)
              */
             @Override
             protected void onPostExecute(Spanned s) {
