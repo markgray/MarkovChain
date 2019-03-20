b172d80886073fdc73ddcd776421b5c637d0c0d1
markgray <mark.t.gray@gmail.com>
2019-03-20 06:40:19 -0400

    More fiddling about with Kotlin TestBed

--- mobile/src/main/java/com/example/android/markovchain/TestBed.kt
+++ mobile/src/main/java/com/example/android/markovchain/TestBed.kt
@@ -2,11 +2,22 @@ package com.example.android.markovchain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import com.example.android.common.Shakespeare
 
 class TestBed : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_bed)
+        val myButton: Button = findViewById(R.id.button)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Shakespeare.SONNETS)
+        val listView: ListView = findViewById(R.id.listview)
+        listView.adapter = adapter
+
+
+
     }
 }
