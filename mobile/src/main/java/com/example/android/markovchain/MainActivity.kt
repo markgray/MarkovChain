4e771d3e0eeeb3e5543ada8cc006fe636f0fbe3e
markgray <mark.t.gray@gmail.com>
2019-10-04 07:00:41 -0400

    Edited comments.

--- mobile/src/main/java/com/example/android/markovchain/MainActivity.kt
+++ mobile/src/main/java/com/example/android/markovchain/MainActivity.kt
@@ -9,9 +9,9 @@ import android.widget.TextView
 
 /**
  * This is the main Activity which launches all the other Activity's when their "launching Button"
- * is clicked. Its layout file (R.layout.activity_main) consists only of a vertical
- * `LinearLayout` wrapped in a `ScrollView` and `Button`'s and `TextView`'s
- * are added to the `LinearLayout` using java code.
+ * is clicked. Its layout file (R.layout.activity_main) consists only of a vertical `LinearLayout`
+ * wrapped in a `ScrollView` and `Button`'s and `TextView`'s are added to the `LinearLayout` using
+ * java code.
  */
 @Suppress("MemberVisibilityCanBePrivate")
 class MainActivity : Activity() {
@@ -19,10 +19,10 @@ class MainActivity : Activity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.activity_main. We
-     * initialize our variable `ViewGroup linearLayout` by finding the view in our content view
-     * with id R.id.linear_layout. We then use our `addButton` method to add buttons that will
-     * launch each of our activities to `linearLayout` as well as using our method `addText`
-     * to add a `TextView` to separate the "featured" Activity's from experimental ones added
+     * initialize our `val linearLayout` by finding the [ViewGroup] in our content view with the
+     * id R.id.linear_layout. We then use our [addButton] method to add buttons that will
+     * launch each of our activities to `linearLayout` as well as using our method [addText]
+     * to add a [TextView] to separate the "featured" Activity's from experimental ones added
      * to the end of `linearLayout`.
      *
      * @param savedInstanceState we do not override `onSaveInstanceState` so do not use this
@@ -46,29 +46,26 @@ class MainActivity : Activity() {
     }
 
     /**
-     * Constructs and adds a `Button` to the `ViewGroup parent` designed to launch a
-     * different Activity when it is clicked. First we initialize our variable `Button button`
-     * with a new instance, then we set its text to the parameter `String description`, and we
-     * set its `OnClickListener` to an anonymous class which will (when the `Button` is
-     * clicked) create an `Intent` to launch the `Activity` whose `Class` is given
-     * in our parameter `Class destination` and start that `Activity`. Finally we add
-     * `Button button` to the parameter `ViewGroup parent` (the `LinearLayout` in
-     * our layout file in our case).
+     * Constructs and adds a [Button] to the [ViewGroup] parameter [parent] designed to launch a
+     * different Activity when it is clicked. First we initialize our `val button` to a new instance
+     * of [Button], then we set its text to the [String] parameter [description], and we
+     * set its `OnClickListener` to a lambda which will (when the [Button] is clicked) create an
+     * [Intent] to launch the [Activity] whose `Class` is given in our [Class] parameter [destination]
+     * and start that [Activity]. Finally we add  the [Button] `button` to the parameter [parent]
+     * (the `LinearLayout` in our layout file in our case).
      *
-     * @param destination Activity Class to be started by an Intent we create and start
-     * when the Button is clicked
-     * @param description text for the Button
-     * @param parent the LinearLayout we are adding the Button to using **ViewGroup.addView**
+     * @param destination [Class] of the [Activity] to be started by the Intent we create
+     * @param description text for the [Button]
+     * @param parent the [ViewGroup] we are adding the [Button] to using **ViewGroup.addView**
      */
     fun addButton(destination: Class<*>, description: String, parent: ViewGroup) {
         val button = Button(this)
         button.text = description
         /*
-         * Called when the button is clicked. We initialize our variable `Intent intent`
-         * with a new new instance whose application package `Context` is that of 'this'
-         * instance of `MainActivity` and whose component class that is to be executed is
-         * the `Class destination` parameter of the `addButton` method. We then launch
-         * the activity specified by `intent`.
+         * Called when the button is clicked. We initialize our `val intent` with a new new instance
+         * of `Intent` whose application package `Context` is that of 'this' instance of `MainActivity`
+         * and whose component class that is to be executed is the `Class` parameter `destination`
+         * of the `addButton` method. We then launch the activity specified by `intent`.
          *
          * Parameter: `View` that was clicked
          */
@@ -80,10 +77,10 @@ class MainActivity : Activity() {
     }
 
     /**
-     * Adds a `TextView` displaying the `String text` to the `ViewGroup parent`.
-     * First we initialize our variable `TextView mText` with a new instance, then we set its
-     * text to our parameter `String text`, and finally we add it to our parameter
-     * `ViewGroup parent`.
+     * Adds a [TextView] displaying the [String] parameter [text] to the [ViewGroup] parameter
+     * [parent]. First we initialize our `val mText` with a new instance of [TextView], then we
+     * set its text to our [String] parameter [text], and finally we add it to our [ViewGroup]
+     * parameter [parent].
      *
      * @param text text to display in the `TextView` we add to `ViewGroup parent`
      * @param parent `ViewGroup` to add our `TextView` to
