5f04d749a924c66299d6e1a082f1cc520a1ec489
markgray <mark.t.gray@gmail.com>
2019-10-06 06:13:42 -0400

    Edited comments.

--- mobile/src/main/java/com/example/android/markovchain/BibleBookmark.kt
+++ mobile/src/main/java/com/example/android/markovchain/BibleBookmark.kt
@@ -27,24 +27,21 @@ class BibleBookmark : DialogFragment() {
      * Called to do initial creation of a `DialogFragment`. This is called after `onAttach(Activity)`
      * and before `onCreateView(LayoutInflater, ViewGroup, Bundle)`.
      *
-     *
      * Note that this can be called while the fragment's activity is still in the process of being
      * created. As such, you can not rely on things like the activity's content view hierarchy being
      * initialized at this point.  If you want to do work once the activity itself is created, see
-     * [.onActivityCreated].
-     *
+     * [onActivityCreated].
      *
      * First we call through to our super's implementation of `onCreate`, then we initialize our
-     * field `String mLabel` set to the `String` stored under the key "label" in our argument
-     * `Bundle`, and `String mText` set to the `String` stored under the key "text"
-     * in that `Bundle`. Finally we set the style of our `DialogFragment` to be STYLE_NORMAL
-     * (for a reason which escapes me right now).
+     * [String] field [mLabel] to the `String` stored under the key "label" in the arguments that
+     * were supplied when our fragement was instantiated, and initialize our [String] field [mText]
+     * to the `String` stored under the key "text" in the argument `Bundle`. Finally we set the
+     * style of our `DialogFragment` to be STYLE_NORMAL (for a reason which escapes me right now).
      *
      * @param savedInstanceState We do not override `onSaveInstanceState` so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         mLabel = arguments!!.getString("label")
         mText = arguments!!.getString("text")
@@ -55,30 +52,28 @@ class BibleBookmark : DialogFragment() {
 
     /**
      * Called to have the fragment instantiate its user interface view. This will be called between
-     * [.onCreate] and [.onActivityCreated].
+     * [onCreate] and [onActivityCreated].
      *
+     * If you return a View from here, you will later be called in [onDestroyView] when the view is
+     * being released.
      *
-     * If you return a View from here, you will later be called in [.onDestroyView] when the
-     * view is being released.
+     * First we initialize the [View] `val v` by using our [LayoutInflater] parameter [inflater] to
+     * inflate our layout file R.layout.bible_bookmark using our [ViewGroup] parameter [container]
+     * for its layout parameters without attaching to it, then we initialize the [View] `var tv` by
+     * finding the view in `v` with id R.id.label and set its text to our [String] field [mLabel],
+     * and then we set `tv` to the view in `v` with id R.id.text and set its text to our [String]
+     * field [mText]. Finally we locate the "DISMISS" Button in `v` (view with id R.id.dismiss) and
+     * set its `OnClickListener` to a lambda which will dismiss this [BibleBookmark] dialog when
+     * clicked, while updating the label and text used by the `BibleDialog` which launched us and to
+     * which we will be returning to.
      *
-     *
-     * First we initialize `View v` by using our parameter `LayoutInflater inflater` to
-     * inflate our layout file R.layout.bible_bookmark using our parameter `ViewGroup container`
-     * for layout params without attaching to is, then we initialize `View tv` by finding the
-     * view in `v` with id R.id.label and set its text to our field `String mLabel`, and
-     * then we set `tv` to the view with id R.id.text and set its text to our field `String mText`.
-     * Finally we locate the "DISMISS" Button (view with id R.id.dismiss) and set its `OnClickListener`
-     * to an anonymous class which will dismiss this `BibleBookmark DialogFragment` when clicked,
-     * while updating the label and text used by the `BibleDialog` which launched us and to which
-     * we will be returning to.
-     *
-     * @param inflater  The LayoutInflater object that can be used to inflate any views in the fragment,
-     * @param container If non-null, this is the parent view that the fragment's UI should be attached to.
-     * The fragment should not add the view itself, but this can be used to generate
-     * the `LayoutParams` of the view.
+     * @param inflater [LayoutInflater] object that can be used to inflate any views in the fragment
+     * @param container If non-null, this is the parent view that the fragment's UI will be attached
+     * to. The fragment should not add the view itself, but this can be used to generate the
+     * `LayoutParams` of the view.
      * @param savedInstanceState We do not override `onSaveInstanceState` so do not use
      *
-     * @return Return the View for the fragment's UI, or null.
+     * @return Return the [View] for the fragment's UI, or null.
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.i(TAG, "onCreateView called")
@@ -113,10 +108,10 @@ class BibleBookmark : DialogFragment() {
         const val TAG = "BibleBookmark"
 
         /**
-         * Create a new `BibleBookmark DialogFragment`. We initialize `BibleBookmark f`, with
-         * a new instance, then initialize `Bundle args` with a new instance, add our parameter
-         * `String label` to `args` using the index "label", add our parameter `String text`
-         * to `args` using the index "text", and then set the arguments for `f` to `args`.
+         * Create a new [BibleBookmark] instance. We initialize [BibleBookmark] `val f`, with
+         * a new instance, then initialize [Bundle] `val args` with a new instance, add our
+         * [String ]parameter [label] to `args` using the index "label", add our [String] parameter
+         * [text] to `args` using the index "text", and then set the arguments for `f` to `args`.
          * Finally we return `f` to our caller.
          *
          * @param label Canonical Bible citation for the verse being bookmarked
