ece09db5fe5305b8a7a529d6438b9a8891dbd187
markgray <mark.t.gray@gmail.com>
2019-10-10 06:55:23 -0400

    Edited comments.

--- mobile/src/main/java/com/example/android/markovchain/ShakespeareRecycler.kt
+++ mobile/src/main/java/com/example/android/markovchain/ShakespeareRecycler.kt
@@ -12,29 +12,29 @@ import android.util.Log
 @Suppress("MemberVisibilityCanBePrivate")
 class ShakespeareRecycler : Activity() {
     /**
-     * The `RecyclerView` in our layout
+     * The [RecyclerView] in our layout which display the text of Shakespeare's sonnets
      */
     lateinit var mRecyclerView: RecyclerView
     /**
-     * `LayoutManager` for our `RecyclerView` (a `LinearLayout` instance)
+     * [RecyclerView.LayoutManager] for our [RecyclerView] (a `LinearLayoutManager` instance)
      */
     lateinit var mLayoutManager: RecyclerView.LayoutManager
     /**
-     * Adapter for our `RecyclerView`
+     * Adapter for our [RecyclerView]
      */
     lateinit var mAdapter: StringArrayAdapter
 
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.activity_shakespeare_recycler.
-     * Next we initialize our field `mLayoutManage` with an instance of `LinearLayoutManager`.
-     * We locate the view with id R.id.shakespeare_recyclerview in order to initialize our field
-     * `RecyclerView mRecyclerView` to it. We create a new instance of `StringArrayAdapter`
-     * using the `SONNETS` field of our `Shakespeare` class (the `String[]` array
-     * containing Shakespeare's sonnets), and `mLayoutManager` as arguments to the constructor
-     * and initialize our field `mAdapter` to it. Finally we set the adapter of our field
-     * `RecyclerView mRecyclerView` to `mAdapter`, and set the layout manager for it to
-     * our field `RecyclerView.LayoutManager mLayoutManager`.
+     * Next we initialize our [RecyclerView.LayoutManager] field [mLayoutManager] with an instance of
+     * [LinearLayoutManager]. We locate the view with id R.id.shakespeare_recyclerview in order to
+     * initialize our [RecyclerView] field [mRecyclerView] to it. We create a new instance of
+     * [StringArrayAdapter] using the `SONNETS` field of our [Shakespeare] class (the `String[]` array
+     * containing Shakespeare's sonnets) as its dataset, and [mLayoutManager] as its `LayoutManager`
+     * and initialize our [StringArrayAdapter] field [mAdapter] to it. Finally we set the adapter of
+     * our [RecyclerView] field [mRecyclerView] to [mAdapter], and set the layout manager for it to
+     * our [RecyclerView.LayoutManager] field [mLayoutManager].
      *
      * @param savedInstanceState we do not override onSaveInstanceState so ignore this
      */
